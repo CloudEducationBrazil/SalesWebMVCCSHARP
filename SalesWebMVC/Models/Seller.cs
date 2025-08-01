4b65d531b040a310c11d133a5c25ff14a0b8ab0a
Heleno Cardoso <helenocardosofilho@gmail.com>
2025-08-01 14:10:01 -0300

    Others entities and second migration

---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,52 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller() {}
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            /*          
+                     double sum = 0;
+      
+                     foreach (SalesRecord sales in Sales)
+                        {
+                            if ( sales.Date >= initial && sales.Date <= final)
+                                sum += sales.Amount;
+                        }
+
+                         return sum;
+            */
+                        
+            return Sales.Where<SalesRecord>(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
